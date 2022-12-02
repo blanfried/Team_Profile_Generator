b6ca2a50b1dbdaa1dd86cefea4f022b159d59eef
Baer Lanfried <blanfried@yahoo.com>
2022-12-02 08:20:57 +0000

    general renderer of employee and extensions

--- src/lib/renderer.js
+++ src/lib/renderer.js
@@ -4,3 +4,24 @@ const fs = require("fs");
 
 // Importing from html files
 const pageDir = path.resolve( "./src/pages");
+
+const render = employees => {
+      const html = [];
+    
+    
+      html.push(employees
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => renderManager(manager))
+      );
+      html.push(employees
+        .filter(employee => employee.getRole() === "Engineer")
+        .map(engineer => renderEngineer(engineer))
+      );
+      html.push(employees
+        .filter(employee => employee.getRole() === "Intern")
+        .map(intern => renderIntern(intern))
+      );
+    
+      return renderMain(html.join(""));
+    
+    };
