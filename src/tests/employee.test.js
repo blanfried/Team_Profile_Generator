f0e27a95065a11f7418f9cfdcaab80c6b83f912c
Baer Lanfried <blanfried@yahoo.com>
2022-12-02 08:29:16 +0000

    added employee test (functions properly)

---
+++ src/tests/employee.test.js
@@ -0,0 +1,28 @@
+
+// Employee test cases 
+const Employee = require("../lib/Employee");
+
+test("creates new employee", () => {
+    const newEmployee = new Employee();
+    expect(typeof(newEmployee)).toBe("object");
+});
+test("getName() ", () => {
+    const nameTest = "John";
+    const newEmployee = new Employee(nameTest);
+    expect(newEmployee.getName()).toBe(nameTest);
+});
+test("getId() works properly", () => {
+    const idTest = 1;
+    const newEmployee = new Employee("John", idTest);
+    expect(newEmployee.getId()).toBe(idTest);
+  });
+  test("getRole() works properly", () => {
+    const roleTest = "Employee";
+    const newEmployee = new Employee("John");
+    expect(newEmployee.getRole()).toBe(roleTest);
+  });
+  test("getSchool() works properly", () => {
+    const schoolTest = "School";
+    const newEmployee = new Employee("John");
+    expect(newEmployee.getSchool()).toBe(schoolTest);
+  });
