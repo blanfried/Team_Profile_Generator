d77bc4b121dd5ec67f7850ec9c77f52a45a8287e
Baer Lanfried <blanfried@yahoo.com>
2022-12-02 08:07:34 +0000

    added employeee constructor

---
+++ index.js
@@ -0,0 +1,10 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+const Manager = require("./src/lib/manager");
+const Intern = require("./src/lib/intern");
+const Engineer = require("./src/lib/engineer");
+
+const render = require("./src/lib/renderer")
+
---
+++ src/lib/employee.js
@@ -0,0 +1,32 @@
+// Employee constructor
+
+class Employee {
+      constructor(name, id, email, school) {
+          this.name = name;
+          this.id = id;
+          this.email = email;
+          this.school = school;
+  
+      }
+  
+      getName() {
+          return this.name;
+      };
+  
+      getId() {
+          return this.id;
+      };
+  
+      getEmail() {
+          return this.email;
+      }
+  
+      getRole() {
+          return "Employee";
+      }
+      getSchool() {
+          return "School";
+      }
+  };
+  
+  module.exports = Employee;
