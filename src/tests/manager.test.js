6800b4d4641683fb8aaeda8fcfa2ce0fdbf77fef
Baer Lanfried <blanfried@yahoo.com>
2022-12-02 08:32:28 +0000

    added manager test (functions properly)

---
+++ src/tests/manager.test.js
@@ -0,0 +1,8 @@
+// Manager test cases 
+const Manager = require("../lib/employee");
+
+test("getRole() should be correct position", () => {
+    const testRole = "Manager";
+    const newManager = new Manager("Nafisa", 1, "nafisa@team.com", "manager01");
+    expect(newManager.getRole() == testRole);
+  });
